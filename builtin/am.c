be9dd7fd2b18a94f0020af9163d130399aa22229
Nicolas Morey-Chaisemartin <NMoreyChaisemartin@suse.com>
2017-11-14 16:30:04 +0100

    am: use am_clean helper from am_next
    Signed-off-by: Nicolas Morey-Chaisemartin <NMoreyChaisemartin@suse.com>

--- builtin/am.c
+++ builtin/am.c
@@ -1118,17 +1118,7 @@ static void am_next(struct am_state *state)
 {
 	struct object_id head;
 
-	FREE_AND_NULL(state->author_name);
-	FREE_AND_NULL(state->author_email);
-	FREE_AND_NULL(state->author_date);
-	FREE_AND_NULL(state->msg);
-	state->msg_len = 0;
-
-	unlink(am_path(state, "author-script"));
-	unlink(am_path(state, "final-commit"));
-
-	oidclr(&state->orig_commit);
-	unlink(am_path(state, "original-commit"));
+	am_clean(state);
 
 	if (!get_oid("HEAD", &head))
 		write_state_text(state, "abort-safety", oid_to_hex(&head));
